3655b285cc5c22be3f27bcee7bd198b791008a8b
Greg Munker <gmunker@gmail.com>
2021-01-18 11:08:12 -0700

    fixed connection URL for mongodb

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(`mongodb://${process.env.MONGODB_URL}`, {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
